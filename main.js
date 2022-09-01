bae6e3f023e13c5fa5ab25d4236720243eab7938
alex1991tikhomirov <alex1991tikhomirov@gmail.com>
2022-09-01 11:26:47 +0300

    Accomplished project

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+window.addEventListener('load', resetMe, false)
 
 const billInput = document.getElementById('bill');/*need to get data from the innput to a variable and add a eventlistener*/
 bill.addEventListener('change', calculateTip);
@@ -7,7 +8,7 @@ peopleInput.addEventListener('change', calculateTip);
 
 const tipPercent = document.getElementsByClassName('percent');//getting percent buttons to a node list and adding an eventlistener
 
-let tip;
+
 
 const button = document.getElementsByClassName('button');
 const butArr = Array.from(button);
@@ -15,27 +16,28 @@ butArr.forEach((el) =>{
     el.onclick = calculateTip;
 });
 
-
+let tip;
 const percentArr = Array.from(tipPercent);
 percentArr.forEach(function(element){
     element.addEventListener('click', ()=>{
-        tip = parseInt(element.innerHTML);
+        tip = parseFloat(element.innerHTML);
         console.log(tip);
     });
 });
 
 
 const reset = document.getElementById('reset');
-reset.addEventListener('click', resetMe, [false]);
+reset.addEventListener('click', resetMe, false);
 
 function resetMe(){
 
     tip = parseInt(percentArr[2].innerHTML);
+    
     console.log(tip);
     reset.style.backgroundColor = '#0D686D';
     reset.style.color = '#065257';
-    billInput.value = '';
-    peopleInput.value = '';
+    billInput.value = '1';
+    peopleInput.value = '1';
     document.getElementById('tipPerPerson').textContent = `$0.00`;
     document.getElementById('tipTotal').textContent = `$0.00`;
 
@@ -46,26 +48,16 @@ function resetMe(){
 
 function calculateTip(){
 
-    // reset.style.backgroundColor = '#26C2AD';
+    reset.style.backgroundColor = '#26C2AD';
     
     let bill = parseFloat(billInput.value).toFixed(2);// variable with input data converted to decimal number
-    
     let person = parseInt(peopleInput.value);
-
     let tipAmount = (bill*(tip/100)).toFixed(2);
-
     let total = parseFloat(tipAmount + bill).toFixed(2);
-
     let tipPerPerson = parseFloat(tipAmount / person).toFixed(2);
-    
-    document.getElementById('tipPerPerson').textContent = `\$${tipPerPerson}`;
 
+    document.getElementById('tipPerPerson').textContent = `\$${tipPerPerson}`;
     document.getElementById('tipTotal').textContent = `\$${total}`;
     
     }
-
-
-
-
-
-
+calculateTip();
